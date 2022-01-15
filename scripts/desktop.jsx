33d3674fe1c14ca5448056ea770b5137dfae148b
nenikitov <nenikitov@mail.ru>
2022-01-15 11:18:21 -0500

    Implemented correct gaps

--- scripts/desktop.jsx
+++ scripts/desktop.jsx
@@ -8,15 +8,18 @@ export class Desktop extends React.Component {
             <article className='desktop'>
                 <img src={this.props.wallpaper}></img>
                 <article className='contents'>
+                    <p style={{color: 'white'}}>Top bar</p>
                     <section className='visible-clients'>
                         <section className='client-column'>
+                            {this.props.apps.visible[0].contents}
                         </section>
                         <section className='client-column'>
+                            {this.props.apps.visible[0].contents}
+                            {this.props.apps.visible[0].contents}
                         </section>
                     </section>
                 </article>
             </article>
         );
     }
-    //{this.props.apps.visible[0].contents}
 }
